da2c35f7903131850f9fc8f4c8d68555ddb5b23b
hafizhrizkyzaid <hafizh.zaid@gmail.com>
2013-09-27 15:43:12 +0700

    Praktikum

---
+++ Praktikum/Praktikum2.java
@@ -0,0 +1,9 @@
+public class Praktikum2 {
+	public static void main (String [] args){
+
+			for (int x = 0; x < 7; x++){
+			
+					System.out.println("Heloo, Worlds we are using loops");
+			}
+		}
+	}
---
+++ Praktikum/Praktikum3.java
@@ -0,0 +1,8 @@
+public class Praktikum3 {
+	public static void main (String [] args){
+
+			while(true) {
+				System.out.println("Heloo World we are using loops");
+			}
+		}
+	}
